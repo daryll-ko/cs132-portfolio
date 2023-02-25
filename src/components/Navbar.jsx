1d81d204539ec6c52a6b3687b0d7901703303783
Daryll Ko <daryll.ko@gmail.com>
2023-02-25 20:07:56 +0800

    fix: remove debug style from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
   const sections = ["Top", "Overview", "Data", "Methods", "Results", "Team"];
   return (
-    <div className="debug flex items-center justify-center gap-7 bg-[#282828] py-3 md:gap-10 md:py-5">
+    <div className="flex items-center justify-center gap-7 bg-[#282828] py-3 md:gap-10 md:py-5">
       {sections.map((section) => (
         <div className="text-sm font-bold text-white md:text-base">
           {section}
