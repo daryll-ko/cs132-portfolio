2841a5b5e8d687d81fcc46345611407d705a52cb
Daryll Ko <daryll.ko@gmail.com>
2023-02-28 16:31:28 +0800

    feat: add progress indicator to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,29 @@
+import { motion, useScroll, useSpring } from "framer-motion";
+
 function Navbar() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress);
+
   const sections = ["Top", "Overview", "Data", "Methods", "Results", "Team"];
+
   return (
-    <div className="sticky top-0 z-50 flex items-center justify-center gap-7 bg-[#282828] py-3 md:gap-10 md:py-5">
-      {sections.map((section, index) => (
-        <a
-          href={`#${section.toLowerCase()}`}
-          className="transition-all hover:scale-110"
-        >
-          <div
-            className="text-sm font-bold text-white md:text-base"
-            key={index}
+    <div className="sticky top-0 z-50">
+      <div className="flex items-center justify-center gap-7 bg-[#282828] py-3 md:gap-10 md:py-5">
+        {sections.map((section, index) => (
+          <a
+            href={`#${section.toLowerCase()}`}
+            className="transition-all hover:scale-110"
           >
-            {section}
-          </div>
-        </a>
-      ))}
+            <div
+              className="text-sm font-bold text-white md:text-base"
+              key={index}
+            >
+              {section}
+            </div>
+          </a>
+        ))}
+      </div>
+      <motion.div className="h-2 origin-left bg-red-500" style={{ scaleX }} />
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      display: ["Open Sans"],
+      display: ["Roboto Mono"],
     },
     extend: {},
   },
