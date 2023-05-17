cc9b420160868fdac7c287378b9b1df07609ec31
Daryll Ko <daryll.ko@gmail.com>
2023-05-17 23:58:22 +0800

    feat: add Exploration cards

--- src/components/Exploration.tsx
+++ src/components/Exploration.tsx
@@ -2,18 +2,35 @@ import { motion } from "framer-motion";
 
 import TwitterImage from "../assets/twitter.png";
 import PythonImage from "../assets/python.png";
+import StockImage from "../assets/sample-gradient.jpeg";
 
 function Exploration() {
   const data = [
     {
       image: TwitterImage,
-      heading: "Collection",
-      text: "We scraped our data ipsum dolor sit.",
+      heading: "Preprocessing",
+      text: (
+        <p>
+          We applied all kinds of techniques, from{" "}
+          <b>handling missing values</b> to doing some{" "}
+          <b>natural language processing</b>!
+        </p>
+      ),
     },
     {
       image: PythonImage,
-      heading: "Processing",
-      text: "Python is a programming language.",
+      heading: "Visualization",
+      text: (
+        <p>
+          We used <b>Plotly</b> to generate <b>interactive</b> plots about
+          different kinds of relationships within our data.
+        </p>
+      ),
+    },
+    {
+      image: StockImage,
+      heading: "Storytelling",
+      text: "Our exploration process is laid out in a way to keep the reader engaged in the process.",
     },
   ];
   return (
@@ -22,18 +39,22 @@ function Exploration() {
       className="border-b-2 border-solid border-black bg-[#efefef] py-20 px-6 sm:px-10 lg:px-16"
     >
       <h1 className="mb-8 text-center text-3xl font-bold md:text-5xl">
-        Which tools did we use?
+        How did we explore our data?
       </h1>
       <h2 className="text-center text-lg text-gray-700 md:text-2xl">
-        [description about our methodology]
+        Using a{" "}
+        <span className="bg-gradient-to-r from-[#a59186] to-[#c99039] bg-clip-text font-extrabold text-transparent drop-shadow-md">
+          Jupyter notebook
+        </span>
+        !
       </h2>
       <div className="mx-auto my-16 flex max-w-5xl flex-wrap justify-center gap-10 text-center">
         {data.map(({ image, heading, text }, index) => (
           <div
             key={index}
-            className="h-96 w-96 rounded-md border border-solid border-black bg-gray-100"
+            className="h-80 w-80 rounded-md border border-solid border-black bg-gray-100"
           >
-            <div className="flex h-[70%] items-center justify-center border-b border-solid border-black">
+            <div className="flex h-[50%] items-center justify-center border-b border-solid border-black">
               <motion.img
                 src={image}
                 alt="Sample gradient"
@@ -41,9 +62,9 @@ function Exploration() {
                 className="h-full"
               />
             </div>
-            <div className="flex h-[30%] flex-col justify-center gap-2">
-              <h3 className="text-2xl font-bold">{heading}</h3>
-              <p>{text}</p>
+            <div className="flex h-[50%] flex-col justify-center gap-2">
+              <h3 className="text-xl font-bold">{heading}</h3>
+              <p className="px-3 py-1 text-sm">{text}</p>
             </div>
           </div>
         ))}
