159c4c7599094cd3f5b11691b8ae12cdae44ca80
Daryll Ko <daryll.ko@gmail.com>
2023-05-17 23:45:11 +0800

    feat: add Collection cards

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -3,23 +3,46 @@ import { motion } from "framer-motion";
 import SampleGradient from "../assets/sample-gradient.jpeg";
 import SampleGradient2 from "../assets/sample-gradient-2.jpeg";
 import SampleGradient3 from "../assets/sample-gradient-3.jpeg";
+import Counter from "./Counter";
 
 function Collection() {
   const data = [
     {
       img: SampleGradient,
-      heading: "Topic",
-      text: "Lorem ipsum dolor sit amet.",
+      heading: "Using twint",
+      text: (
+        <p>
+          We used the{" "}
+          <a
+            href="https://github.com/twintproject/twint"
+            className="text-blue-600"
+          >
+            <b>twint</b>
+          </a>{" "}
+          Python library to collect tweets. We first put all our tweets in a{" "}
+          <b>fodder spreadsheet</b>.
+        </p>
+      ),
     },
     {
       img: SampleGradient2,
-      heading: "Keywords",
-      text: "Lorem ipsum dolor sit amet?",
+      heading: "Tweet curation",
+      text: (
+        <p>
+          We selected tweets in our <b>fodder spreadsheet</b> that aligned with
+          our project interests and moved them to the <b>main spreadsheet</b>.
+        </p>
+      ),
     },
     {
       img: SampleGradient3,
-      heading: "Tools",
-      text: "Lorem ipsum dolor sit amet!",
+      heading: "Manual labeling",
+      text: (
+        <p>
+          We manually labeled the <b>Account type</b>, <b>Content type</b>, and{" "}
+          <b>Reasoning</b> columns.
+        </p>
+      ),
     },
   ];
   return (
@@ -28,29 +51,34 @@ function Collection() {
       className="border-b-2 border-solid border-black bg-[#efefef] py-20 px-6 sm:px-10 lg:px-16"
     >
       <h1 className="mb-6 text-center text-3xl font-bold md:text-5xl">
-        Where did our data come from?
+        How did we collect our data?
       </h1>
       <h2 className="text-center text-lg text-gray-700 md:text-2xl">
-        [description about our data]
+        With the help of{" "}
+        <span className="bg-gradient-to-r from-[#336c9b] to-yellow-500 bg-clip-text font-extrabold text-transparent drop-shadow-md">
+          Python
+        </span>
+        !
       </h2>
       <div className="mx-auto my-16 flex max-w-5xl flex-wrap justify-center gap-10 text-center">
         {data.map(({ img, heading, text }, index) => (
           <div
             key={index}
-            className="h-72 w-72 overflow-hidden rounded-md border border-solid border-black bg-gray-100"
+            className="h-80 w-80 overflow-hidden rounded-md border border-solid border-black bg-gray-100"
           >
-            <div className="h-[65%] overflow-hidden">
+            <div className="h-[50%] overflow-hidden">
               <img src={img} alt="Sample gradient" className="object-cover" />
             </div>
-            <div className="flex h-[35%] flex-col justify-center gap-2">
-              <h3 className="text-2xl font-bold">{heading}</h3>
-              <p>{text}</p>
+            <div className="flex h-[50%] flex-col justify-center gap-2">
+              <h3 className="text-xl font-bold">{heading}</h3>
+              <p className="px-3 py-1 text-sm">{text}</p>
             </div>
           </div>
         ))}
       </div>
       <p className="mb-8 text-center text-lg text-gray-700 md:text-2xl">
-        [additional description about our data]
+        We collected <Counter from={0} to={150} /> data points to be used for
+        exploration.
       </p>
       <div className="flex justify-center">
         <motion.button
@@ -62,7 +90,7 @@ function Collection() {
             href="https://docs.google.com/spreadsheets/d/1xeTTNx1zVFSfcaIIqL7B2uOQ3gnYt_L5z_ioXfwIL5Q/edit#gid=107810933"
             target="_blank"
           >
-            Look at our data!
+            Look at our sheets!
           </a>
         </motion.button>
       </div>
